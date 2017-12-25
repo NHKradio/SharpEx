8c89c4e0fa32bbab9a243cf885d0fab96eedf1d9
NHKradio <bigstoneblues@gmail.com>
2017-12-25 15:55:46 +1000

    godno

---
+++ SharpEx/InputBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpEx.Support.Forms;
+
+namespace SharpEx
+{
+    namespace Support
+    {
+        public static class InputBox
+        {
+            public static string Show(string title = "", string caption = "", string tbtext = "")
+            {
+                InputBoxForm f = new InputBoxForm(title, caption, tbtext);
+                if (f.ShowDialog() == DialogResult.OK)
+                {
+                    return InputBoxForm.output;
+                }
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ SharpEx/InputBoxForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SharpEx
+{
+    namespace Support
+    {
+        namespace Forms
+        {
+            public partial class InputBoxForm : Form
+            {
+                public static string output;
+
+                public InputBoxForm(string title = "", string caption = "", string tbtext = "")
+                {
+                    InitializeComponent();
+                    output = string.Empty;
+                    Text = title;
+                    label1.Text = caption;
+                    textBox1.Text = tbtext;
+                    button1.Click += Button1_Click;
+                    button1.DialogResult = DialogResult.OK;
+                }
+
+                private void Button1_Click(object sender, EventArgs e)
+                {
+                    output = textBox1.Text;
+                }
+            }
+        }
+    }
+}
